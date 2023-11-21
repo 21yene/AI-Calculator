6ef6b1c1b2cfe9522567d3150cf7e4578b9c6271
Abraham Mekuria <35862886+21yene@users.noreply.github.com>
2023-11-21 22:57:43 +0300

    again

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('This my first trial for nodejs app in docker. ansdfdsgd abraham is here')
+  res.send('This my first trial for nodejs app in docker. hello again here')
 })
 
 app.listen(port, () => {
